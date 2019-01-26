b684e51c30311977f2c7bd89855c005713e9380e
gabrielkerekes <gabriel.kerekes.ml@gmail.com>
2019-01-26 16:17:57 +0100

    App at the beginning

---
+++ MySortedCollection/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MySortedCollection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new List<int> { 6, 3, 7, 1, 2, 8 };
+            var sortedCollection = new SortedCollection(numbers);
+
+            Console.WriteLine(sortedCollection);
+        }
+    }
+}
---
+++ MySortedCollection/SortedCollection.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MySortedCollection
+{
+    public class SortedCollection
+    {
+        private List<int> items;
+
+        public SortedCollection()
+        {
+
+        }
+
+        public SortedCollection(List<int> items)
+        {
+            this.items = items;
+            BubbleSort();
+        }
+
+        public SortedCollection(int[] items)
+        {
+            this.items = items.ToList();
+            BubbleSort();
+        }
+
+        public void BubbleSort()
+        {
+            for (var i = 0; i < items.Count; i++)
+            {
+                for (var j = 0; j < items.Count - 1; j++)
+                {
+                    if (items[j] > items[j + 1])
+                    {
+                        var tmp = items[j];
+                        items[j] = items[j + 1];
+                        items[j + 1] = tmp;
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(",", items);
+        }
+    }
+}
